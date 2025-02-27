363511bb1fad920f3dfe6775ba952e9c9f1f48fa
cyniu88 <bazz5@wp.pl>
2025-02-27 21:44:11 +0100

    Update mqtt.py

--- comfobridge/mqtt.py
+++ comfobridge/mqtt.py
@@ -20,11 +20,11 @@ class Mqtt:
 
     def sensor_publish(self, sensor, value):
         logger.debug("Publishing %s = %s to MQTT broker", sensor.name, value)
-        asyncio.create_task(self.client.publish(self.sensor_topic + "/" + sensor.name.replace(" ", ""), to_mqtt_format(value)))
+        asyncio.create_task(self.client.publish(self.sensor_topic + "/" + sensor.name.replace(" ", ""), to_mqtt_format(value), retain=True))
 
     def publish(self, topic, value):
         logger.debug("Publishing %s = %s to MQTT broker", topic, value)
-        asyncio.create_task(self.client.publish(topic, value))
+        asyncio.create_task(self.client.publish(topic, value, retain=True))
 
     async def __aenter__(self):
         await self.client.__aenter__()
